311efb07f99f0bf5476153cc4bdbadc35b705694
James Marcil <james@jamesmarcil.com>
2024-02-13 20:43:55 -0500

    Initial implementation of undo/redo stack.

--- command/command.py
+++ command/command.py
@@ -1,4 +1,5 @@
 import sys
+import abc
 
 class Player(object):
     x:int
@@ -11,20 +12,48 @@ class Player(object):
     def __str__(self) -> str:
         return "The player is located at ({x},{y}).".format(x=self.x, y=self.y)
 
-    def move_up(self) -> None:
-        self.y += 1
+class ICommand(abc.ABC):
+    @abc.abstractmethod
+    def execute(self, player:Player) -> None:
+        pass
 
-    def move_down(self) -> None:
-        self.y -= 1
+    @abc.abstractmethod
+    def undo(self, player:Player) -> None:
+        pass
 
-    def move_left(self) -> None:
-        self.x -= 1
+class MoveDownCommand(ICommand):
+    def execute(self, player: Player) -> None:
+        player.y -= 1
+
+    def undo(self, player:Player) -> None:
+        player.y += 1
+
+class MoveUpCommand(ICommand):
+    def execute(self, player: Player) -> None:
+        player.y += 1
     
-    def move_right(self) -> None:
-        self.x += 1
+    def undo(self, player:Player) -> None:
+        player.y -= 1
+
+class MoveLeftCommand(ICommand):
+    def execute(self, player: Player) -> None:
+        player.x -= 1
+    
+    def undo(self, player:Player) -> None:
+        player.x += 1
+
+class MoveRightCommand(ICommand):
+    def execute(self, player: Player) -> None:
+        player.x += 1
+
+    def undo(self, player:Player) -> None:
+        player.x -= 1
 
 if __name__ == "__main__":
     the_player = Player()
+
+    undo_stack = []
+    redo_stack = []
 
     while True:
         try:
@@ -33,17 +62,36 @@ if __name__ == "__main__":
             if result == "print":
                 print(the_player)
             elif result == "move up":
-                the_player.move_up()
+                command = MoveUpCommand()
+                command.execute(the_player)
+                undo_stack.append(command)
             elif result == "move down":
-                the_player.move_down()
+                command = MoveDownCommand()
+                command.execute(the_player)
+                undo_stack.append(command)
             elif result == "move left":
-                the_player.move_left()
+                command = MoveLeftCommand()
+                command.execute(the_player)
+                undo_stack.append(command)
             elif result == "move right":
-                the_player.move_right()
+                command = MoveRightCommand()
+                command.execute(the_player)
+                undo_stack.append(command)
             elif result == "undo":
-                pass
+                if len(undo_stack) > 0:
+                    command = undo_stack.pop()
+                    command.undo(the_player)
+                    redo_stack.append(command)
+                else:
+                    print("Nothing to undo!")
             elif result == "redo":
-                pass
+                if len(redo_stack) > 0:
+                    command = redo_stack.pop()
+                    command.execute(the_player)
+                else:
+                    print("Nothing to redo!")
+            else:
+                print("Unrecognized input: {input}".format(input=result))
 
         except KeyboardInterrupt:
             sys.exit(0)
